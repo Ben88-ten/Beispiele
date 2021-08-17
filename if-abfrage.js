3fc61cae97d499fe27508cf3c0329c1685c360fb
Benjamin <74067465+Ben88-ten@users.noreply.github.com>
2021-08-17 20:02:25 +0200

    Add files via upload

---
+++ if-abfrage.js
@@ -0,0 +1,43 @@
+" use strict"
+/*
+let a = true; 
+
+// let a = false;
+
+/*
+if (a = true){
+    alert("Hallo")
+}
+
+else {
+    alert("Tschüs")
+}
+
+
+if (a) {
+    console.log("Hallo Welt")
+}
+
+*/
+
+let zahl1 = 4
+let zahl2  = 6 
+
+if (zahl1 <= zahl2){
+    console.log("Zahl eins ist nicht kleinder als Zahl 2 ")
+}
+
+
+else if (zahl1 >= zahl2){
+    console.log("Zahl eins ist kleiner als Zahl 2 ")
+}
+
+else {
+    console.log(" else ")
+}
+
+let students = ["Max", "Moriz"]
+if (students.indexOf("Erik" === -1)){
+    console.log("Erik ist nicht in dem Studiengang")
+}
+
